7ff8a81268cead1e6ab9fcdb82b9d389b137c2ce
Freed900 <katbatir@gmail.com>
2021-10-28 22:44:14 +0300

    Full Project
    ready for review

---
+++ Client/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static int port = 5000;
+        static string address = "127.0.0.1";
+        
+        static void Main(string[] args)
+        {
+            do
+            {
+
+                string number;
+                int value;
+                do
+                {
+                    Console.Clear();
+                    Console.Write("Введите число:");
+                    number = Console.ReadLine();
+
+                }
+                while (!int.TryParse(number, out value));
+                string[] words = GetWordsFromServer(value);
+                foreach (var word in words)
+                    Console.Write(word + " ");
+                Console.WriteLine("\nДля выхода нажмите ESC");
+
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+
+        static string[] GetWordsFromServer(int count)
+        {
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(ipPoint);
+
+
+                byte[] data = BitConverter.GetBytes(count);
+                socket.Send(data);
+
+                data = new byte[256];
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0;
+
+                do
+                {
+                    bytes = socket.Receive(data, data.Length, 0);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (socket.Available > 0);
+
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+                return builder.ToString().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return new string[0];
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,69 @@
+﻿
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Linq;
+
+namespace Server
+{
+    class Program
+    {
+        static int port = 5000;
+
+        static string[] words = {"Гусь", "Утка", "Трава", "Вор", "Ябеда", "Свинья", "Лебедь", "Птичка", "Книга"}; 
+
+        static void Main(string[] args)
+        {
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                listenSocket.Bind(ipPoint);
+
+                listenSocket.Listen(10);
+
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+                    Socket handler = listenSocket.Accept();
+                    int bytes = 0;
+                    byte[] data = new byte[256];
+
+                    do
+                    {
+                        bytes = handler.Receive(data);
+                    }
+                    while (handler.Available > 0);
+                    byte[] newArr = new byte[bytes];
+                    Array.Copy(data, newArr, bytes);
+
+                    int value= BitConverter.ToInt32(newArr);
+                    if(value>=0 && value<words.Length)
+                    {
+                        StringBuilder wordsToSend = new StringBuilder();
+                        Random random = new Random();
+                        for (int i = 0; i < value; i++)
+                            wordsToSend.Append( words[random.Next(0,words.Length)]+",");
+                        handler.Send(Encoding.Unicode.GetBytes(wordsToSend.ToString()));
+
+                        Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " +value);
+                    }
+                    else
+                    {
+                        handler.Send(Encoding.Unicode.GetBytes("parameter is not an number"));
+                    }
+              
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
